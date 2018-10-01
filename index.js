0256f27b264997e950c5480a8032a65a93a0ae65
Aran Mason <AranM@hpa.org.nz>
2018-10-02 12:55:30 +1300

    Added test data to api

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const crypto = require('crypto');
-//const request = require('request');
 const path = require('path');
-var OAuth = require('oauth-1.0a');
-var request = require('request');
+const test = require('./test-data/data.js');
+
 require('dotenv').config();
 
 const app = express();
@@ -26,55 +24,12 @@ app.get('/api/passwords', (req, res) => {
   console.log(`Sent ${count} passwords`);
 });
 
-app.get('/api/trademe', (req, res)=>{
+app.get('/api/trademe/flatmate-placeholder', (req, res)=>{
 
-    const oauth = OAuth({
-      consumer: {
-        key: process.env.TRADEME_CLIENT_KEY,
-        secret: process.env.TRADEME_CLIENT_SECRET
-      },
-      signature_method: 'HMAC-SHA1',
-      hash_function(base_string, key) {
-        return crypto.createHmac('sha1', key).update(base_string).digest('base64');
-      }
-    });
 
-    // trademe.setToken({
-    //   key: process.env.TRADEME_TOKEN,
-    //   secret: process.env.TRADEME_SECRET
-    // });
+    var data = test.flatmate;
 
-    // trademe.get(process.env.TRADEME_URI + '/Search/Property/Rental.json', function(err, response, data){
-    //   if(err) console.error(error);
-
-    //   console.log('Code:', response) 
-
-    //   res.json(data);
-    // });
-
-    const token = {
-      key: process.env.TRADEME_TOKEN,
-      secret: process.env.TRADEME_SECRET
-    };
-
-    var request_data = {
-        url: process.env.TRADEME_URI + '/Search/Property/Rental.json',
-        method: 'GET',
-        data: 'Content-Type: \'application/json\''           
-    }
-
-    
-
-    request({
-        url: request_data.url,
-        method: request_data.method,
-        form: oauth.authorize(request_data, token),
-        headers: Object.assign(oauth.toHeader(oauth.authorize(request_data, token)), {'content-type': 'application/json'})
-      }, function(error, response, body) {
-          if(error) console.error(error);
-          //console.log(response);
-          res.json(body);
-      });
+    res.json(data);
     
 });
 
